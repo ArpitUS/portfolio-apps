8129e9b3629f8efd2ce7c8f162470c6970061732
ArpitUS <arpitusrivastava@hotmail.com>
2025-10-23 12:55:11 +0530

    chore(go): add go.sum and finalize module for agro-watch-iot build

--- agro-watch-iot/go.mod
+++ agro-watch-iot/go.mod
@@ -1,3 +1,5 @@
 module github.com/arpitus/agro-watch-iot
 
 go 1.23.4
+
+require github.com/gorilla/mux v1.8.1
--- agro-watch-iot/main.go
+++ agro-watch-iot/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	_ "github.com/gorilla/mux" // temporary dummy import to trigger go.sum
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, `{"status":"ok","service":"agro-watch-iot"}`)
+}
+
 func main() {
-	fmt.Println("AgroWatch IoT service is running.")
+	http.HandleFunc("/health", healthHandler)
+	fmt.Println("agro-watch-iot running on :8080")
+	_ = http.ListenAndServe(":8080", nil)
 }
